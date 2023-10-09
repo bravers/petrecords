482e2f28ac9f8e1718da88efd9dc404b0479a4a6
bravers <bravers@yandex.ru>
2023-10-09 17:39:44 +0300

    add Cat class

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+/*
+Организовать учёт кошек. Про каждую кошку мы знаем кличку и возраст(в годах). Нужно что бы была возможность добавлять, редактировать и удалять кошек из БД. Нужна возможность фильтровать кошек по возрасту.
+
+*/
+
+// mysqli
+
+
+class Cat
+{
+    public $name = "";
+    public $age = 0;
+
+    public function getCat()
+    {
+        echo "sdfsd";
+    }
+
+    public function addCat()
+    {
+
+    }
+
+    public function updateCat()
+    {
+
+    }
+
+    public function removeCat()
+    {
+
+    }
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cats</title>
+</head>
+<body>
+    <h1>Cats</h1>
+</body>
+</html>
